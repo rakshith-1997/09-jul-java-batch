a7b87183bf7ae84bb9ac9e01cae481f40cf402ce
rakshith-1997 <108971062+rakshith-1997@users.noreply.github.com>
2022-07-10 16:53:31 +0530

    now

--- workspace/java_1st class/src/practise/practise_a.java
+++ workspace/java_1st class/src/practise/practise_a.java
@@ -4,7 +4,10 @@ public class practise_a {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("abc");
+		System.out.println("abc");
+		int a=50;
+		int b=40;
+		System.out.println(a+b);
 
 	}
 
