6e9d9f2fb3ef37b0d43f17b478ad2ee5047b0c9f
rakshith-1997 <108971062+rakshith-1997@users.noreply.github.com>
2022-07-10 12:44:24 +0530

    java class 10/07/2022

---
+++ workspace/java_1st class/src/besantjavaprogram/maratahalli.java
@@ -0,0 +1,25 @@
+package besantjavaprogram;
+
+public class maratahalli {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("welcome");
+		//System.out.print("java");
+		//System.out.print(" class");
+		//System.out.println("12"+"13");
+	    //System.out.println("3+5");
+	    //System.out.println(3+"5");
+	    //System.out.println(52/5);
+	    //System.out.println(52%5);
+	    //System.out.println(52*5);
+		 var a ="java";
+		var b ="besant";
+		System.out.println(a+" "+b);
+		int a1 = 10;
+		int b1 = 20;
+		System.out.println(a1+b1);
+
+	}
+
+}
